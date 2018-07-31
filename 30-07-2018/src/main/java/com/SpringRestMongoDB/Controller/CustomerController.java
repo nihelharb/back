d254c61ccf94f911b7ffeacb6b63b27bc9b75c8e
asus <asus@DESKTOP-OOURG63>
2018-07-31 17:54:20 +0200

    bb

--- 30-07-2018/src/main/java/com/SpringRestMongoDB/Controller/CustomerController.java
+++ 30-07-2018/src/main/java/com/SpringRestMongoDB/Controller/CustomerController.java
@@ -34,6 +34,7 @@ public class CustomerController {
 	
 		List<Customer> customers = new ArrayList<>();
 		repository.findAll().forEach(customers::add);
+		System.out.println("hh");
  
 		return customers;
 	}
